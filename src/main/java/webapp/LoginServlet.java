ffb76ba946aa9b8eb411d7a12346003bf747ae1f
Aneesh B S <Aneesh.B.S@kyndryl.com>
2023-10-04 12:03:55 +0530

    updated the webpage content

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -29,6 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 //3. doGet(HttpServletRequest request, HttpServletResponse response)
 //4. How is the response created?
 
+
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
